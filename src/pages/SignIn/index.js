11809182436269c71e1a5e4cfc74620895f3aec6
Rogerio <rogerio.francis@gmail.com>
2019-09-30 16:11:43 -0400

    Persist Redux

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Image } from 'react-native';
-import Background from '~/components/Background';
+
+import { useDispatch } from 'react-redux';
 import logo from '~/assets/logo.png';
+import Background from '~/components/Background';
+import { signInRequest } from '~/store/modules/auth/actions';
+
 import {
   Container,
   Form,
@@ -13,9 +17,18 @@ import {
 } from './styles';
 
 export default function SignIn({ navigation }) {
+  const dispatch = useDispatch();
   const passwordRef = useRef();
 
-  function handleSubmit() {}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit() {
+    console.tron.log(email);
+
+    console.tron.log(password);
+    dispatch(signInRequest(email, password));
+  }
 
   return (
     <Background>
@@ -31,6 +44,8 @@ export default function SignIn({ navigation }) {
             placeholder="Digite seu e-mail"
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
 
           <FormInput
@@ -40,8 +55,10 @@ export default function SignIn({ navigation }) {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
-          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignUp')}>
